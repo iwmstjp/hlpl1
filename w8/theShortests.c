555ba45178288c9242595318cfa7e0c3e5a445e9
iwmstjp <miwaura95@gmail.com>
2023-04-23 22:21:06 +0200

    add the average and the shorests

---
+++ w8/theShortests.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char *argv[]){
+    int min = strlen(argv[1]);
+    for(int i = 1; i < argc; i++){
+        if(strlen(argv[i]) < min){
+            min = strlen(argv[i]);
+        }
+    }
+    for(int i = 1; i < argc; i++){
+        if(strlen(argv[i]) == min){
+            printf("%s\n", argv[i]);
+        }
+    }
+    return 0;
+}
